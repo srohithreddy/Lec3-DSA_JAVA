679ae83af372313fe93ddd8401caf3ea7ed3e1c8
srohithreddy <122622306+srohithreddy@users.noreply.github.com>
2023-06-29 20:51:02 +0530

    Add files via upload

---
+++ PrimitiveDT.java
@@ -0,0 +1,23 @@
+package coreJava;
+public class PrimitiveDT {
+
+	public static void main(String[]args)
+	{
+		int x=5;
+		int y=7;
+		System.out.println("X is:" +x);
+		System.out.println("Y is:" +y);
+		
+		byte b=10;
+		System.out.println("B is:" +b);
+		
+		byte b2=(byte)129;
+		System.out.println("B is:" + b2);
+		
+		int i=b;
+		System.out.println("I is:" +i);
+		
+		
+	}
+
+}
